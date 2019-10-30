3a9b94d693e950830fd6ee1704b77309fb3a1a81
Ana Chernov <anastasia.chernov@gmail.com>
2019-10-30 19:01:28 -0400

    Created equations for time calculations for next arrival and minutes away

--- logic.js
+++ logic.js
@@ -50,36 +50,42 @@ btn.addEventListener("click", function(e) {
     frequency,
     firstTime
   });
-
-  // https://firebase.google.com/docs/database/web/read-and-write
-
-  // function setTrainRef(name, destination, frequency, firstTime) {
-  //   firebase
-  //     .database()
-  //     .ref("trains/" + name)
-  //     .set({
-  //       name,
-  //       destination,
-  //       frequency,
-  //       firstTime
-  //     });
-  // }
-
-  // // Change what is saved in firebase
-  // setTrainRef(name, destination, frequency, firstTime);
 });
 
 database.ref().on("child_added", function(snapshot) {
   var sv = snapshot.val();
 
-  // console.log(snapshot.val());
-  // console.log(snapshot.key);
+  // How often the train comes
+  var tFrequency = sv.frequency;
+  console.log("freq" + tFrequency);
 
-  // // Console.loging the last user's data
-  // console.log(sv.name);
-  // console.log(sv.destination);
-  // console.log(sv.frequency);
-  // console.log(sv.firstTime);
+  // Current time
+  var currentTime = moment().format("HH:mm");
+  console.log("CURRENT TIME: " + currentTime);
+
+  // First train time
+  var firstTrain = sv.firstTime;
+  console.log("first time: " + firstTrain);
+
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+  console.log("first time conv: " + firstTimeConverted);
+
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % tFrequency;
+  console.log(tRemainder);
+
+  // Minute Until Train
+  var tMinutesTillTrain = tFrequency - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
 
   var newRow = $("<tr>");
 
@@ -87,16 +93,26 @@ database.ref().on("child_added", function(snapshot) {
   var newNameCell = $("<td>").text(sv.name);
   var newDestinationCell = $("<td>").text(sv.destination);
   var newFreqCell = $("<td>").text(sv.frequency);
-  var newFirstCell = $("<td>").text(sv.firstTime);
 
-  // btnCounter++;
+  ////////////// These will be mathy calculations for the last two columns
+  // var newNextArr = $("<td>");
+  // var newMinAway = $("<td>");
 
-  // // Change the HTML to reflect
+  /////////// Change the HTML to reflect
   // newRow.append(newDelete);
   newRow.append(newNameCell);
   newRow.append(newDestinationCell);
   newRow.append(newFreqCell);
-  newRow.append(newFirstCell);
+  // newRow.append(newNextArr);
+  // newRow.append(newMinAway);
 
   $("#train-sched").append(newRow);
 });
+
+// Code this app to calculate when the next train will arrive; this should be relative to the current time
+
+// Input validation - make sure that the user filled in all the fields
+// Make sure the first train time is in military format, don't let the user submit if it isn't
+// Delete function if time
+
+// Current Time
